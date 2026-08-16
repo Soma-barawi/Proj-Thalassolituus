f328421d1ba0a4d35247176ddaa0ab69b20fbd07
Soma Barawi <sm871954@dal.ca>
2026-08-15 23:33:13 -0300

    Update download_ncbi_genomes.sh

--- Scripts/download_ncbi_genomes.sh
+++ Scripts/download_ncbi_genomes.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # NCBI Datasets
 # Use datasets to download biological sequence data across all domains of life from NCBI
 # Use dataformat to convert metadata from JSON Lines format to other formats
@@ -12,5 +10,12 @@ mkdir -p NCBI-Oceanospirillaceae
 unzip -q NCBI-Oceanospirillaceae.zip -d NCBI-Oceanospirillaceae
 
 # Another useful python script for downloading genomes from RefSeq/ Genbank
+
+conda create -n ncbi_env python=3.9
+conda update -n base -c conda-forge conda
+conda activate ncbi_env
+pip install ncbi-genome-download
+
+ncbi-genome-download --genera Thalassolituus --section refseq --formats fasta, protein-fasta bacteria
 ncbi-genome-download --genera genomes.txt --formats fasta --assembly-levels complete,chromosome,scaffold,contig bacteria
 ncbi-genome-download bacteria --assembly-accessions accessions.txt --output-folder genomes_downloaded 
