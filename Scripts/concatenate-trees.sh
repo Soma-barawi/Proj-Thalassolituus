bbe3d1f2e28d64ed94fe2e1a858a08b2aebd93d0
Soma Barawi <sm871954@dal.ca>
2026-08-15 22:05:40 -0300

    Add script to concatenate Nif alignments and run IQ-TREE
    This script concatenates Nif protein alignments and runs IQ-TREE for phylogenetic analysis.

---
+++ Scripts/concatenate-trees.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p concatenated-alignments
+cd nif-alignments
+
+# Concatenate the Nif protein alignments
+AMAS.py concat -i nifH_aligned.faa nifD_aligned.faa nifK_aligned.faa -f fasta -d aa --part-format raxml \ 
+  -p ../concatenated-alignments/nifHDK_partitions.txt -t ../concatenated-alignments/nifHDK_concat.faa
+AMAS.py concat -i nifH_aligned.faa nifD_aligned.faa nifK_aligned.faa nifE_aligned.faa nifN_aligned.faa nifB_aligned.faa -f fasta -d aa --part-format raxml \ 
+  -p ../concatenated-alignments/nifHDKENB_partitions.txt -t ../concatenated-alignments/nifHDKENB_concat.faa
+cd ..
+
+# Run IQ-TREE
+iqtree3 -s concatenated-alignments/nifHDK_concat.faa -p concatenated-alignments/nifHDK_partitions.txt -m MFP+MERGE -B 1000 --alrt 1000 -T AUTO --prefix nifHDK_concat_AA
+iqtree3 -s concatenated-alignments/nifHDKENB_concat.faa -p concatenated-alignments/nifHDKENB_partitions.txt -m MFP+MERGE -B 1000 --alrt 1000 -T AUTO --prefix nifHDKENB_concat_AA
